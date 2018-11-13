1d48486e6a828f40d242c227aa8c84a877f5450b
Rick <56639624@qq.com>
2018-11-13 17:29:18 +0800

    add pdf document

--- ex3/ex3.m
+++ ex3/ex3.m
@@ -1,1 +1,8 @@
 %% Octave/MATLAB script that steps you through part 1
+%% Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
+
+clear; close all; clc
+
+load('ex3data1.mat');
+
+displayData(X, 100);
