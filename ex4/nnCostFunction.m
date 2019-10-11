a15e89757c62000199d5be1bc4e3a4b52296cf38
rick <56639624@qq.com>
2019-10-11 16:38:28 +0800

    add needed data

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -21,45 +21,43 @@ a2 = sigmoid(X * Theta1');
 a2 = [ones(m, 1) a2];      
 a3 = sigmoid(a2 * Theta2');  
 
-Theta1_temp = [zeros(size(Theta1,1),1) Theta1(:,2:end)];
+Theta1_temp = [zeros(size(Theta1,1),1) Theta1(:,2:end)];% 把theta(1)拿掉，不参与正则化
 Theta2_temp = [zeros(size(Theta2,1),1) Theta2(:,2:end)];
 
 temp1 = sum(Theta1_temp .^ 2);
 temp2 = sum(Theta2_temp .^ 2);
 
-cost = Y .* log(a3) + (1 - Y) .* log((1 - a3));
-J = -1 / m * sum(cost(:)) + lambda/(2*m) * (sum(temp1(:)) + sum(temp2(:))); 
+cost = Y .* log(a3) + (1 - Y) .* log((1 - a3));% h(x) = a3
+J = -1 / m * sum(cost(:)) + lambda/(2*m) * (sum(temp1(:)) + sum(temp2(:))); % 代价函数为什么把0-9类的损失都包含进去？
 
 %% Graident
-delta_1 = zeros(size(Theta1));
-delta_2 = zeros(size(Theta2));
+Delta_1 = zeros(size(Theta1)); % 25 * 401
+Delta_2 = zeros(size(Theta2)); % 10 * 26
 
 for i = 1:m
 	% step 1
-	a_1 = X(i, :)';
-	z_2 = Theta1 * a_1;
-	a_2 = sigmoid(z_2);
-	a_2 = [1; a_2];
-	z_3 = Theta2 * a_2;
-	a_3 = sigmoid(z_3);
+	a_1 = X(i, :)'; % 401 * 1
+	z_2 = Theta1 * a_1; % 25 * 1
+	a_2 = sigmoid(z_2); % 25 * 1
+	a_2 = [1; a_2]; % 26 * 1
+	z_3 = Theta2 * a_2; % 10 * 1
+	a_3 = sigmoid(z_3); % 10 * 1
 	% step 2
-	err_3 = zeros(num_labels, 1);
+	delta_3 = zeros(num_labels, 1); % 10 * 1
 	for k = 1:num_labels
-		err_3(k) = a_3(k) - (y(i) == k);
+		delta_3(k) = a_3(k) - (y(i) == k);
 	end
 	% step 3
-	err_2 = Theta2' * err_3;
-	err_2 = err_2(2:end) .* sigmoidGradient(z_2);
+	delta_2 = Theta2' * delta_3; % 26 * 1
+	delta_2 = delta_2(2:end) .* sigmoidGradient(z_2); % 25 * 1
 	% step 4
-	delta_2 = delta_2 + err_3 * a_2';
-	delta_1 = delta_1 + err_2 * a_1';
+	Delta_2 = Delta_2 + delta_3 * a_2';
+	Delta_1 = Delta_1 + delta_2 * a_1';
 end
 
 % step 5
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
-Theta1_grad = 1 / m * delta_1 + lambda/m * Theta1_temp;
-Theta2_grad = 1 / m * delta_2 + lambda/m * Theta2_temp;
+Theta1_grad = 1 / m * Delta_1 + lambda/m * Theta1_temp;
+Theta2_grad = 1 / m * Delta_2 + lambda/m * Theta2_temp;
 
 % Unroll gradients
 gradient = [Theta1_grad(:) ; Theta2_grad(:)];
